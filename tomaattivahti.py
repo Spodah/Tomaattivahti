46ee1ea14905ac4f1457057cbf96056152898321
Spodah <Spodah@users.noreply.github.com>
2017-03-24 09:30:41 +0200

    Add files via upload

--- tomaattivahti.py
+++ tomaattivahti.py
@@ -2,6 +2,7 @@
 import requests
 import serial
 from time import sleep
+import datetime
 
 def serReadLine(ser):
 	s = ser.readline(500)
@@ -28,24 +29,28 @@ def send_alert():
             "https://api.mailgun.net/v3/sandboxd4b3c74cc0cd43c3af0c44891a743e61.mailgun.org/messages",
             auth=("api", "key-64a1ddae467588fd957b9955876332c6"),
             data={"from": "Tomaattivahti <postmaster@sandboxd4b3c74cc0cd43c3af0c44891a743e61.mailgun.org>",
-                    "to": ["leo.koskiluoma@myy.haaga-helia.fi"],
+                    "to": ["juuso.puroila@myy.haaga-helia.fi"],
                     "subject": "Alert",
                     "text": alertmessage})
             
 
 ser = serial.Serial(port="/dev/ttyACM0", baudrate=115200, timeout=1)
+now = datetime.datetime.now()
+noon = now.replace(hour=8, minute=0, second=0, microsecond=0)
+pastnoon = now.replace(hour=16, minute=0, second=0, microsecond=0)
+#timetest = now.replace(hour=12, minute=30, second=0, microsecond=0)
 
 while True:
     s = serReadLine(ser)
-    if s == None:
+    if s == None: #these lines ensure the output is valid
         s = "Noneerror"
     s = s.split()
     if len(s)==4:
-        if isfloat(s[0]) & isfloat(s[1]) & isfloat(s[2]):
+        if isfloat(s[0]) & isfloat(s[1]) & isfloat(s[2]) & isfloat(s[3]):
             temp = float(s[1])
             airhumidity = float(s[0])
             soilhumidity = float(s[2])
-            light = float(s[3])
+            lightlevel = float(s[3])
             alertmessage = ""
             if temp<10:
                 alertmessage = "Alert! Low temperature "
@@ -61,16 +66,17 @@ while True:
                 alertmessage = alertmessage+ "Alert! Soil too wet "
             elif soilhumidity < 300:
                 alertmessage = alertmessage+ "Alert! Soil too dry "
-            if light > 800:
-                alertmessage = alertmessage+ "Alert! Plant lights are off "
+            if noon<now<pastnoon:
+                if lightlevel > 500:
+                    alertmessage = alertmessage+ "Alert! Light broken "
                                     
             if alertmessage != "":
-                r = send_alert()
+                #r = send_alert()
                 print (alertmessage)
             else:
                 print ("no alert " +s[0] + s[1] + s[2])
             print (s)
-            break
+            break #the program stops after valid input has been processed, otherwise keeps trying again
         else:
             print (s)
             sleep(0.1)
